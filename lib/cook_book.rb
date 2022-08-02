046897c98ed3da62e729392b4df10351def4f9a6
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-02 10:26:33 -0700

    Add calorie method to cookbook class and edit caloriee test in cookbook spec

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -15,4 +15,10 @@ class CookBook
       end
     end.flatten.uniq
   end
+
+  def highest_calorie_meal
+    @recipes.max_by do |recipe|
+      recipe.total_calories
+    end
+  end
 end
--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -5,6 +5,8 @@ require './lib/cook_book'
 RSpec.describe CookBook do
   let(:ingredient1) {Ingredient.new({name: "Cheese", unit: "C", calories: 100})}
   let(:ingredient2) {Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})}
+  let(:ingredient3) {Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})}
+  let(:ingredient4) {Ingredient.new({name: "Bun", unit: "g", calories: 75})}
   let(:recipe1) {Recipe.new("Mac and Cheese")}
   let(:recipe2) {Recipe.new("Cheeseburger")}
   let(:cookbook) {CookBook.new}
@@ -20,8 +22,6 @@ RSpec.describe CookBook do
   end
 
   it 'can return ingredient names' do
-    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
-    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
     recipe1.add_ingredient(ingredient1, 2)
@@ -32,9 +32,14 @@ RSpec.describe CookBook do
     expect(cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
   end
 
-  xit 'can return highest calorie meal' do
+  it 'can return highest calorie meal' do
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
     expect(cookbook.highest_calorie_meal).to eq(recipe2)
   end
 end
