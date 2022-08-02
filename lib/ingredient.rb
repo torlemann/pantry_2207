319314577546788bd93c6c55aabf181e9c268d9e
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-02 08:17:26 -0700

    Add ingredient class

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,9 @@
 class Ingredient
+
+  attr_reader :name, :unit, :calories
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @unit = attributes.fetch(:unit)
+    @calories = attributes.fetch(:calories)
+  end
 end
