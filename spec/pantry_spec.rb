fded23dd8aad995921c0f5c75b91a0f4bfac623a
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-02 10:32:11 -0700

    Refactor pantry spec and add enough ingred test

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -2,30 +2,37 @@ require './lib/ingredient'
 require './lib/pantry'
 
 RSpec.describe Pantry do
+  let(:ingredient1) {Ingredient.new({name: "Cheese", unit: "oz", calories: 50})}
+  let(:ingredient2) {Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})}
+  let(:pantry) {Pantry.new}
 
   it 'exists and has readable attributes'do
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    pantry = Pantry.new
     expect(pantry).to be_a(Pantry)
     expect(pantry.stock).to eq({})
   end
 
   it 'can check pantry stock' do
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    pantry = Pantry.new
     expect(pantry.stock_check(ingredient1)).to eq(0)
   end
 
   it 'can restock' do
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    pantry = Pantry.new
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
     expect(pantry.stock_check(ingredient1)).to eq(15)
 
     pantry.restock(ingredient2, 7)
     expect(pantry.stock_check(ingredient2)).to eq(7)
+  end
+
+  it 'can check if pantry has enough ingredients for recipe' do
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    expect(pantry.enough_ingredients_for?(recipe1)).to eq(false)
+
+    pantry.restock(ingredient2, 7)
+    expect(pantry.enough_ingredients_for?(recipe1)).to eq(false)
+
+    pantry.restock(ingredient2, 1)
+    expect(pantry.enough_ingredients_for?(recipe1)).to eq(true)
   end
 end
