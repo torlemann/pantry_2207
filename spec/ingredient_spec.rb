3e054e8cd38f6cfba55cc83ac1dcaf929b8b197a
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-02 08:10:41 -0700

    Initial commit

---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,12 @@
+require './lib/ingredient'
+
+RSpec.describe Ingredient do
+
+  it 'exists and has readable attributes'do
+  ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  expect(ingredient1).to be_a(Ingredient)
+  expect(ingredient1.name).to eq("Cheese")
+  expect(ingredient1.unit).to eq("oz")
+  expect(ingredient1.calories).to eq(50)
+  end
+end
